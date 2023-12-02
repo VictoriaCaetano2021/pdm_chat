d56f2452189f1d4940f06d5114d2513c9347cecb
VictoriaCaetano2021 <102391385+VictoriaCaetano2021@users.noreply.github.com>
2023-12-02 14:00:19 -0300

    Update read_qrcode_screen.dart

--- lib/screens/read_qrcode_screen.dart
+++ lib/screens/read_qrcode_screen.dart
@@ -1,63 +1,63 @@
-import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class QRCodeScannerScreen extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _QRCodeScannerScreenState();
-}
+// class QRCodeScannerScreen extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => _QRCodeScannerScreenState();
+// }
 
-class _QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
-  late final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
-  late QRViewController _controller;
+// class _QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
+//   late final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
+//   late QRViewController _controller;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Leitor QR Code'),
-      ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          QRView(
-            key: _qrKey,
-            onQRViewCreated: _onQRViewCreated,
-          ),
-        ],
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Leitor QR Code'),
+//       ),
+//       body: Stack(
+//         alignment: Alignment.center,
+//         children: [
+//           QRView(
+//             key: _qrKey,
+//             onQRViewCreated: _onQRViewCreated,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      _controller = controller;
-    });
+//   void _onQRViewCreated(QRViewController controller) {
+//     setState(() {
+//       _controller = controller;
+//     });
 
-    controller.scannedDataStream.listen((scanData) {
-      // Aqui você pode tratar os dados do QR code, por exemplo, exibindo-os em um diálogo
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Conteúdo do QR Code'),
-            content: Text(scanData.code),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Fechar'),
-              ),
-            ],
-          );
-        },
-      );
-    });
-  }
+//     controller.scannedDataStream.listen((scanData) {
+//       // Aqui você pode tratar os dados do QR code, por exemplo, exibindo-os em um diálogo
+//       showDialog(
+//         context: context,
+//         builder: (context) {
+//           return AlertDialog(
+//             title: Text('Conteúdo do QR Code'),
+//             content: Text(scanData.code),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                 },
+//                 child: Text('Fechar'),
+//               ),
+//             ],
+//           );
+//         },
+//       );
+//     });
+//   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-}
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+// }
